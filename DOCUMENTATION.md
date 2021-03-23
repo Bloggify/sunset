8ce901d99cb6531a9a35d03f0b49bd27137d06f5
GhitaB <ghitabizau27@gmail.com>
2021-03-23 09:12:38 +0200

    Add files.

--- DOCUMENTATION.md
+++
@@ -1,4 +0,0 @@
-## Documentation
-
-You can see below the API reference of this module.
-
--- app/assets/js/sunset.js
+++
@@ -1,4 +0,0 @@
-const SolarCalc = require("sundown")
-module.exports = (d, lat, lon) => {
-    return SolarCalc(d, +lat, +lon).sunset.time
-}
